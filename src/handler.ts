468daa9462af4cc1d1880be514980e98432fd46b
Branden Kensington Siegle <branden@readywhen.ca>
2025-03-10 11:55:47 -0700

    Update handler.ts

--- src/handler.ts
+++ src/handler.ts
@@ -6,12 +6,13 @@ import type {
 
 /**
  *
- * Using the (Streaming Availability API)[https://docs.movieofthenight.com/resource/shows], return to me a list
- * of movies to watch and the streaming service they’re on. Group them by the service and order them in
+ * Using the (Streaming Availability API (Documentation Link))[https://docs.movieofthenight.com/resource/shows#search-shows-by-filters],
+ * return to me a list of movies to watch and the streaming service they’re on. Group them by the service and order them in
  * increasing order of quality. Order the groups by the average quality.The total length of the movies returned
  * should be less than the number of hours I have available as I hate to not know how something ends.
  *
- * Body will be of type
+ * 
+ * Request Body will be of type
  *  `{“duration”:number,”platforms”:string[], “genre”:string}`
  *
  * Response body is a json object of type
